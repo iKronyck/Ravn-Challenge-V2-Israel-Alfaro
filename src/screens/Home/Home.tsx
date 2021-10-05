0bc9da503e1a05f1b229a1a97b0f4cbf9d898b14
Israel Alfaro <israel.devalfaro@gmail.com>
2021-10-04 23:11:52 -0600

    [Feature] Connected Home screen with data

--- src/screens/Home/Home.tsx
+++ src/screens/Home/Home.tsx
@@ -1,10 +1,28 @@
 import React, {FC} from 'react';
-import {View, Text} from 'react-native';
+import {View} from 'react-native';
+import {Header, LoadingCell, ErrorBoundary, PeopleList} from '@components';
+import {useAllPeople} from '@hooks';
+import {EAppRoutes} from '@router';
+import {THomeScreen} from './Home.type';
 
-export const Home: FC = () => {
+export const Home: FC<THomeScreen> = ({navigation}) => {
+  const {peoples, loading, error} = useAllPeople();
+
+  const onPressGoToPeopleDetail = (personId: string) =>
+    navigation.navigate(EAppRoutes.PEOPLE_INFORMATION, {personId});
+
   return (
     <View>
-      <Text>Hello Home</Text>
+      <Header title="People of Star Wars" />
+      <ErrorBoundary error={error}>
+        <PeopleList
+          onPressGoToPeopleDetail={(personId: string) =>
+            onPressGoToPeopleDetail(personId)
+          }
+          data={peoples}
+        />
+      </ErrorBoundary>
+      {loading ? <LoadingCell /> : null}
     </View>
   );
 };
